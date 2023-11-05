7adeb54cbe24978538288a15e6f36f2976f9d699
Martin Holst Swende <martin@swende.se>
2023-11-05 18:40:04 +0100

    simplify code

--- main.go
+++ main.go
@@ -3,13 +3,17 @@ package main
 import (
 	_ "embed"
 	"fmt"
+	"go/parser"
+	"go/printer"
+	"go/token"
 	"os"
+	"os/exec"
+	"strings"
 	"text/template"
 
 	"github.com/urfave/cli/v2"
 	"golang.org/x/exp/slog"
-	"os/exec"
-	"strings"
+	"golang.org/x/tools/go/ast/astutil"
 )
 
 var (
@@ -154,4 +158,39 @@ func goTidy() error {
 		return err
 	}
 	return nil
+}
+
+func rewriteImport(path, fuzzerName, newImport string) (restoreFn func(), err error) {
+	var fset = token.NewFileSet()
+	astFile, err := parser.ParseFile(fset, path, nil, 0)
+	if err != nil {
+		return nil, err
+	}
+	// Replace import path, if needed
+	if astutil.DeleteImport(fset, astFile, "testing") {
+		astutil.AddImport(fset, astFile, newImport)
+	} else {
+		slog.Warn("No imports to replace", "file", path)
+	}
+	// Write into new file
+	fuzzPath := path + "_fuzz.go"
+	if newFile, err := os.Create(fuzzPath); err != nil {
+		return nil, fmt.Errorf("failed to create new file: %v", err)
+	} else {
+		printer.Fprint(newFile, fset, astFile)
+		newFile.Close()
+		slog.Info("Created new file", "name", newFile.Name())
+	}
+	// Rename old file
+	savePath := fmt.Sprintf("%v.orig", path)
+	slog.Info("Saving original file", "path", savePath)
+	if err := os.Rename(path, savePath); err != nil {
+		return nil, err
+	}
+	restoreFunc := func() {
+		slog.Info("Restoring files", "restoring", path, "removing", fuzzPath)
+		os.Remove(fuzzPath)
+		os.Rename(savePath, path)
+	}
+	return restoreFunc, nil
 }
